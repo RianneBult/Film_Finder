2541223f5c0320b0f21c3769411304d0a52e2311
RianneBult <84838025+RianneBult@users.noreply.github.com>
2022-02-24 10:20:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const listMovies = document.getElementById("list-movies");
+const radioButtons = document.querySelectorAll("input[name=movie-filter]");
+const searchBar = document.getElementById("searchBar");
+
+const addMoviesToDom = (movies) => {
+    movies.map(movie => {
+        const a = document.createElement("a");
+        a.href = "https://www.imdb.com/title/" + movie.imdbID;
+        a.target = "_blank";
+        listMovies.append(a);
+        const moviePoster = document.createElement("img");
+        moviePoster.src = movie.poster;
+        a.append(moviePoster);
+    })
+}
+
+addMoviesToDom(movies);
+
+const filterMovies = (wordInMovies) => {
+    const movieFilter = movies.filter(movie => movie.title.includes(wordInMovies));
+    addMoviesToDom(movieFilter);
+}
+
+const filterLatestMovies = () => {
+    const getLatestMovies = movies.filter(movie => movie.year >= 2014);
+    addMoviesToDom(getLatestMovies);
+}
+
+const eventListeners = () => {
+    radioButtons.forEach((e) => {
+        e.addEventListener("change", handleOnChangeEvent);
+    })
+}
+
+const handleOnChangeEvent = (event) => {
+    listMovies.querySelectorAll("img").forEach(e => e.remove());
+
+    switch (event.target.value) {
+        case "latest-movies":
+            filterLatestMovies();
+            break;
+        case "avenger-movies":
+            filterMovies("Avengers");
+            break;
+        case "x-men-movies":
+            filterMovies("X-Men");
+            break;
+        case "princess-movies":
+            filterMovies("Princess");
+            break;
+        case "batman-movies":
+            filterMovies("Batman");
+            break;
+        default:
+            addMoviesToDom(movies);
+    }
+}
+
+eventListeners();
+
+searchBar.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase();
+    const searchMovieTitle = movies.filter(movie => {
+        return movie.title.toLowerCase().includes(value);
+    })
+    listMovies.querySelectorAll("img").forEach(e => e.remove());
+    addMoviesToDom(searchMovieTitle);
+})
